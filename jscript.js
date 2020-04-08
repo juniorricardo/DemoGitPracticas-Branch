fdc753a7d822a861661b8f7077fa9e9865d80f7e
Junior <neo.junior.16@gmail.com>
2020-04-07 22:20:34 -0300

    Inicio de practicas

--- jscript.js
+++ jscript.js
@@ -1,2 +1,15 @@
-alert("Hola puto!");
-document.write("Este es add texto");
+'use stric'
+
+const list = [
+  { id: 1, task: 'Leer' },
+  { id: 2, task: 'Read' },
+  { id: 3, task: 'Wash' },
+  { id: 4, task: 'Run' }
+]
+
+const render = () => {
+  return list.map(item => {
+      console.log(`Task:` + item.task );
+  })
+}
+render()
